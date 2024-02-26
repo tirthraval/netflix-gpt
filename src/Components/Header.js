6f6b7769b59ca245cb1c3dce2e94bef4ae5d47a4
Tirth Raval <tirthraval1999@gmail.com>
2024-02-25 22:52:52 -0800

    did the protected routiung

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../utils/firbase';
-import { signOut } from 'firebase/auth';
-import { removeUser } from '../utils/userSlice';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { addUser, removeUser } from '../utils/userSlice';
 const Header = () => {
   const user = useSelector(store => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+
+        const { uid, displayName, email, photoURL } = user
+
+        dispatch(addUser({
+          email: email,
+          displayName: displayName,
+          photoURL: photoURL,
+          uid: uid
+        }));
+        navigate('/browse')
+        // ...
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate('/')
+      }
+    })
+
+  }, [])
+
   const handleClick = () => {
     signOut(auth).then(() => {
       // Sign-out successful.
       //disptach and make user null and navigate to the home page
-      dispatch(removeUser(null));
-      navigate('/')
+      
+      
     }).catch((error) => {
       // An error happened.
     });
 
   }
   return (
-    <div className='absolute w-screen py-2  bg-gradient-to-t from-black z-10 flex justify-between'>
+    <div className='absolute w-full py-2  bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className="w-44 mx-4 py-2" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='logo' />
       {
-        (user &&  <div className='flex p-4 justify-between'>
-        <img className='w-20 p-2' src={user?.photoURL} alt="userphoto" />
-        <button className="text-xl font-bold text-white p-3" onClick={handleClick}>
-          (Sign Out)
-        </button>
-      </div>)
+        (user && <div className='flex p-4 justify-between'>
+          <img className='w-20 p-2' src={user?.photoURL} alt="userphoto" />
+          <button className="text-xl font-bold text-white p-3" onClick={handleClick}>
+            (Sign Out)
+          </button>
+        </div>)
       }
-    
-  </div>
+
+    </div>
 
   )
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -64,15 +64,7 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed in 
                     const user = userCredential.user;
-                    const { uid, displayName, email, photoURL } = user
-
-                    dispatch(addUser({
-                        email: email,
-                        displayName: displayName,
-                        photoURL: photoURL,
-                        uid: uid
-                    }));
-                    navvigate('/browse')
+                    
 
 
 
@@ -89,7 +81,7 @@ const Login = () => {
     return (
         <div>
             <Header />
-            <div className="absolute w-screen">
+            <div className="absolute ">
                 <img  src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
             </div>
             <from onSubmit={(e) => e.prevent.default} className='absolute w-3/12 mx-auto left-0 right-0 my-40 bg-black text-white p-12 rounded-lg  bg-opacity-80'>
